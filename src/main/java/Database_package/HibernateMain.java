483963c97d309f8daf1d3125b79ccbda22869038
Christopher Wendl <chris.wendl19@gmail.com>
2017-12-08 15:50:07 +0100

    HibernateMain updated
    Added new public methods to save a game

--- src/main/java/Database_package/HibernateMain.java
+++ src/main/java/Database_package/HibernateMain.java
@@ -28,19 +28,56 @@ public class HibernateMain {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			HibernateMain hb = new HibernateMain();
+			Spiel spiel1 = new Spiel(3,"Heute","Testspiel","Testmap" );
+			Runde runde1 = new Runde(1, "spieler", 0, 10, false, spiel1);
+			
+			hb.SaveGame(spiel1);
+			hb.SaveRound(runde1);
+
+	}
+	
+	public void SaveGame(Spiel spiel) {
 		configureSessionFactory();
 		Session session = null;
 		Transaction tx = null;
 		
+		try {
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+			try {
+			session.save(spiel);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+
+			session.flush();
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+	
+	public void SaveRound(Runde runde) {
+		configureSessionFactory();
+		Session session = null;
+		Transaction tx = null;
 		
 		try {
 			session = sessionFactory.openSession();
 			tx = session.beginTransaction();
-			Spiel spiel1 = new Spiel(3,"Heute","Testspiel","Testmap" );
-			session.save(spiel1);
-			Runde runde1 = new Runde(1, "spieler", 0, 10, false, spiel1);
-			session.save(runde1);
+			try {
+			session.save(runde);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+
 			session.flush();
 			tx.commit();
 		} catch (Exception e) {
