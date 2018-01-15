c95ab758ebe2a971ed962572154a4d98a16bedaf
Christopher Wendl <chris.wendl19@gmail.com>
2018-01-15 18:16:16 +0100

    Implemented basic mapcheck
    Checking if  map has the right size of 4x8 and the right tiletypes between 0 and 2

--- src/main/java/Server/Classes/GameMapData.java
+++ src/main/java/Server/Classes/GameMapData.java
@@ -35,14 +35,31 @@ public class GameMapData {
 	public static Boolean CheckMap(String GameData) {
 		//TODO Extract Map into Array to Check
 		JSONObject obj;
+		String[][] map = new String[4][8];
 		try {
 			obj = new JSONObject(GameData);
 			try {
 				JSONArray arrayMap = obj.getJSONArray("Content");
-				obj = arrayMap.getJSONObject(0);
-				JSONArray contentMap = obj.getJSONArray("Columns");
-				for(int i = 0; i< contentMap.length(); i++) {
-					System.out.println(contentMap.getJSONObject(i));
+				if (arrayMap.length() != 4)
+					return false;
+				for(int i = 0; i < arrayMap.length(); i++) {
+					obj = arrayMap.getJSONObject(i);
+					System.out.println("RownumberY: " + obj.getInt("RowNumber"));
+					JSONArray tmpcontentMap = obj.getJSONArray("Columns");
+					if(tmpcontentMap.length() != 8)
+						return false;
+					for(int j = 0; j< tmpcontentMap.length(); j++) {
+						JSONObject tmpobj = tmpcontentMap.getJSONObject(j);
+						try {
+						if(tmpobj.getInt("TileNumberX") <0 || tmpobj.getInt("TileNumberX") >7)
+							return false;
+						if(tmpobj.getInt("TileType") <0 || tmpobj.getInt("TileType") >2)
+							return false;
+						}catch(JSONException e) {
+							e.printStackTrace();
+						}
+						System.out.println(tmpcontentMap.getJSONObject(j));
+					}
 				}
 				String arrayMapString = new String(arrayMap.toString());
 				System.out.println(arrayMapString);
