20e07e1da2c307836d4b407c9498cfab6fa47eda
Christopher Wendl <chris.wendl19@gmail.com>
2018-01-16 13:46:25 +0100

    Implemented View of Client

--- src/main/java/Client/Model/GameMap.java
+++ src/main/java/Client/Model/GameMap.java
@@ -1,21 +1,29 @@
 package Client.Model;
 
 public class GameMap {
-	public Tile[][] gameMap;
+	public Tile[][] map;
 	
-	public GameMap(Tile[][] gameMap) {
-		this.gameMap=gameMap;
+	public GameMap(Tile[][] map) {
+		this.map=map;
 	}
 	
 	public boolean CastleOn(Integer x, Integer y) {
-		return gameMap[x][y].Castle;
+		return map[x][y].Castle;
+	}
+	
+	public void SetCastleOn(Integer x, Integer y) {
+		map[x][y].SetCastle(true);
 	}
 	
 	public boolean TreasureOn(Integer x, Integer y) {
-		return gameMap[x][y].Treasure;
+		return map[x][y].Treasure;
 	}
 	
 	public void SetWeightOf(Integer x, Integer y, Integer weight) {
-		gameMap[x][y].Setweight(weight);
+		map[x][y].Setweight(weight);
+	}
+	
+	public Integer GetTileTypeOn(Integer x, Integer y) {
+		return map[x][y].GetTileType();
 	}
 }
--- src/main/java/Client/View/GameView.java
+++ src/main/java/Client/View/GameView.java
@@ -1,5 +1,50 @@
 package Client.View;
 
-public class GameView {
 
+import Client.Model.GameMap;
+
+public class GameView {
+	
+	public void Start(String serverResponse) {
+		System.out.println("SWE Game\n");
+		System.out.println("Connecting to server...\n");
+		System.out.println("Server response: " + serverResponse);
+		System.out.println("\nGenerating map....\n");
+	}
+	
+	public void Draw(GameMap gameMap, Integer position, Integer enemyPosition) {
+		System.out.print("\033[H\033[2J");
+		System.out.println("\nThe map:\n");
+		for(int y=0; y< 8; y++) {
+			for(int x=0; x<8; x++) {
+				if(position == x+y)
+					System.out.print('P');
+				else if(enemyPosition == x+y)
+					System.out.print('E');
+				else if(gameMap.CastleOn(x, y))
+					System.out.print('C');
+				else {
+					switch(gameMap.GetTileTypeOn(x, y)) {
+					case 0: System.out.print('o');
+						break;
+					case 1: System.out.print('M');
+						break;
+					case 2: System.out.print('~');
+						break;
+					default: System.out.print('X');
+						break;
+					}
+				}
+			}
+		}
+		System.out.println("Player on Tile : " + position);
+		System.out.println("Enemy on Tile : " + enemyPosition);
+	}
+	
+	public void Finish(boolean won) {
+		if(won)
+			System.out.println("You win the game!!!");
+		else
+			System.out.println("You've lost....");
+	}
 }
